b3d1fc65db8d85062bc283c74e6bc3530bde1b9e
KevinL10 <kevinsliu0@gmail.com>
2022-04-01 09:02:49 -0700

    add uml design diagram

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,7 +3,6 @@ package ui;
 // Driver for the CipherUI
 public class Main {
     public static void main(String[] args) {
-
         //CipherApp consoleApp = new CipherApp();
         new CipherUI();
     }
